d6078cbe4472e4f911c39d988015b9058ed079d7
AustinDHarrison <105564917+ArtPalmer@users.noreply.github.com>
2023-01-30 19:56:01 +0000

    Update (v0.5.5)

--- internal/firebase/auth/firebaseAuth.js
+++ internal/firebase/auth/firebaseAuth.js
@@ -29,6 +29,6 @@ onAuthStateChanged(auth, (user) => {
 
     } else {
     // User is signed out
-    window.location.href = "/portal/?from=" + (location.pathname+location.search).substr(1);
+    window.location.href = "/portal/?from=" + window.location.pathname +"/" +  window.location.search;
     }
 });
